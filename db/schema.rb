2adccd03dbe9965f561dd4854c2e3674744276c5
Daniel Ko <dankomong@gmail.com>
2019-05-27 12:55:35 -0400

    minor change to migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_220915) do
+ActiveRecord::Schema.define(version: 2019_05_24_195542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2019_05_23_220915) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["pokedex_id"], name: "index_pokemons_on_pokedex_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "pokemons", "pokedexes"
