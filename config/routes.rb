c9b9d865414e3130ea9a816de5a46c9f3ff3004d
github username <github email address>
2019-05-28 16:31:22 -0400

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post '/create_team', to:'team#create'
   patch '/update_team/:id', to:'team#update'
   patch '/add_pokemon/:id',to:'team#add_pokemon'
-  post '/log_in',to:'auth#login'
+  post '/login',to:'auth#login'
   get '/teams/index',to:'team#index'
 
   post '/sign_up', to:'user#create'
